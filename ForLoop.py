d519f31ce7b96f23ff2107c2aa19f876813149e3
Titu <titu.socialnetmail@gmail.com>
2023-03-04 18:32:25 +0600

    for loop-1

---
+++ ForLoop.py
@@ -0,0 +1,8 @@
+# ১। একটা পাইথন প্রোগ্রাম লিখতে হবে যা দিয়ে ১৫০০ থেকে ২৭০০ এর ভিতর নাম্বারগুলো খুঁজে বের করতে হবে, যেগুলো মূলত ৭ এবং ৫ দ্বারা বিভাজ্য।
+#solve
+# We define a dic for assign those number
+store = []
+for x in range(1500, 2700):
+    if (x % 7 == 0) and (x % 5 == 0):
+        store.append(str(x))
+print(','.join(store))
